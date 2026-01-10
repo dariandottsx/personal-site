530c959d38b86962dbd92fa8b170be06234c9c53
dariandottsx <darianpan21@gmail.com>
2026-01-09 20:25:11 -0500

    dogfood

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,6 +12,8 @@ const Home = () => {
         <h1>Darian Pan</h1>
         
         <h2>I build products that help creators and startups turn ideas into momentum.</h2>
+        
+        <p>And dogfood every bit of it..</p>
       </section>
       
       <Now />
